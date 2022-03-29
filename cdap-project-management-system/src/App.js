dee3496d5eae3d77f01dd7671687d51cfd724899
buddhi549 <humbleandwise16@gmail.com>
2022-03-29 15:16:42 +0530

    Added Buddhisha line

--- cdap-project-management-system/src/App.js
+++ cdap-project-management-system/src/App.js
@@ -8,11 +8,11 @@ function App() {
     <h3>Hello Chathushka </h3> 
     <h10> by Chathushka</h10> 
     <br></br>
-    <h1> Hello World ! </h1>
+ 
     <h3>Hello Chamath </h3> 
     <h10> by Chamath</h10>
     <h2>Hello by Buddhisha!</h2>
-    <h1>Bruhhhh!</h1>
+    <h1>By Buddhisha</h1>
     </div>
   );
 }
