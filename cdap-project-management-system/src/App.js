80e0035047854d24b590e95864f97f506feb6183
Mr.Chahushka Rodrigo <chathushkarodrigo@gmail.com>
2022-03-29 15:49:16 +0530

    Minor Changes

--- cdap-project-management-system/src/App.js
+++ cdap-project-management-system/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <h3>Hello Chathushka </h3> 
     <h10> by Chathushka</h10> 
     <br></br>
-    <h1> Hello World ! </h1>
+    
     <h3>Hello Chamath </h3> 
     <h10> by Chamath</h10>
     </div>
