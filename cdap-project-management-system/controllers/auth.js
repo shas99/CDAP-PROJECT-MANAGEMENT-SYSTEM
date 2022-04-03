a668c9c540e97bac18aec68361e35263f6731a20
shas99 <pshasvathan1999@gmail.com>
2022-04-04 03:04:26 +0530

    protected route and JWT tokens

--- cdap-project-management-system/controllers/auth.js
+++ cdap-project-management-system/controllers/auth.js
@@ -8,10 +8,7 @@ exports.register = async(req,res,next) => {
             username,email,password
         })
 
-        res.status(201).json({
-            success: true,
-            user
-        })
+        sendToken(user, 201, res)
     }catch(error){
         next(error)
     }
@@ -37,10 +34,7 @@ exports.login = async (req, res, next) => {
             return next(new ErrorResponse("Invalid Credentials",401))
         }
 
-        res.status(200).json({
-            success: true,
-            token: "sdghgsfg",
-        })
+        sendToken(user, 200, res)
     }catch(error){
         res.status(500).json({success:false, error:error.message})
     }
@@ -53,4 +47,9 @@ exports.forgotpassword = (req, res, next) => {
 
 exports.resetpassword = (req, res, next) => {
     res.send("Reset Password Route")
+}
+
+const sendToken = (user, statusCode, res) => {
+    const token = user.getSignedToken()
+    res.status(statusCode).json({success: true,token})
 }
--- cdap-project-management-system/server.js
+++ cdap-project-management-system/server.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(express.json())
 
 app.use('/api/auth', require('./routes/auth'))
+app.use('/api/private', require('./routes/private'))
 
 // Error handler(Should be the last piece of middleware)
 app.use(errorHandler)
