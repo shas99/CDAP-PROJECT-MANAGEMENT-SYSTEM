c4aa68175d43a3c01e2d5197fc03f313470505b0
buddhi549 <humbleandwise16@gmail.com>
2022-04-04 15:15:56 +0530

    Added Private Route

--- cdap-project-management-system/client/src/App.js
+++ cdap-project-management-system/client/src/App.js
@@ -1,4 +1,18 @@
+import PrivateRoute from './components/routing/PrivateRoute';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 const App = () =>{
-  return <div className="App">Hiyo</div>
+  return (
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path='/login' element={<LoginScreen/>} exact/>
+          <Route path='/register'element={<RegisterScreen/>} exact/>
+          <Route path='/forgotpassword' element={<ForgotPasswordScreen/>}exact/>
+         
+
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 export default App;
---
+++ cdap-project-management-system/client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,34 @@
+import { Navigate, Route } from 'react-router-dom';
+import React from 'react';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>{
+        localStorage.getItem("authToken") ? (
+          <Component {...props} />
+        ) : (
+          <Navigate to="/login" />
+        )
+      }}
+    />
+  );
+};
+
+export default PrivateRoute;
+
+/*
+function PrivateRoute({ children, ...rest}) {
+    return (
+        <Route {...rest}
+        render={({ props }) => localStorage.getItem("AuthToken") ? (
+            children
+        ):(
+            <Redirect to={{pathname:"/login"}}/>
+
+            
+        )}/>
+    )
+}
+ export default PrivateRoute;*/
