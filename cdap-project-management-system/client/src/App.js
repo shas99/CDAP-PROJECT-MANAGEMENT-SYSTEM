63569ceafef2df999c42153b8e1b24fba8253562
buddhi549 <humbleandwise16@gmail.com>
2022-04-04 21:18:52 +0530

    Created login and register screens

--- cdap-project-management-system/client/src/App.js
+++ cdap-project-management-system/client/src/App.js
@@ -1,15 +1,27 @@
+
+//routing
 import PrivateRoute from './components/routing/PrivateRoute';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+//screens
+import PrivateScreen from './components/screens/PrivateScreen';
+import LoginScreen from './components/screens/LoginScreen';
+import RegisterScreen from './components/screens/RegisterScreen';
+import ForgotPasswordScreen from './components/screens/ForgotPasswordScreen';
+import ResetPasswordScreen from './components/screens/ResetPasswordScreen';
+
 const App = () =>{
   return (
     <Router>
       <div className="app">
-        <h1>me</h1>
+        
         
         <Routes>
+          <Route path="/" element={<PrivateRoute component={PrivateScreen}/>}/>
           <Route path='/login' element={<LoginScreen/>} exact/>
           <Route path='/register'element={<RegisterScreen/>} exact/>
           <Route path='/forgotpassword' element={<ForgotPasswordScreen/>}exact/>
+          <Route path='/passwordreset/:resetToken' element={<ResetPasswordScreen/>}exact/>
          
 
         </Routes>
