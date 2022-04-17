e123f9846c74a1debb5714b3c7b270e9c6e9a1e5
buddhi549 <humbleandwise16@gmail.com>
2022-04-17 10:39:58 +0530

    Finished styling ViewMarks.js

--- client/src/components/screens/ViewMarks.js
+++ client/src/components/screens/ViewMarks.js
@@ -27,18 +27,47 @@ const ViewMarks = ({history}) =>{
   }, [history])
 
    return(
-    <div className="view-marks">
-    <h1 id="caption">My marks</h1>
-    <div className="card">
-    <div className="container">
-      <h4 id="marks-topic"><b>Milestone 1 </b></h4> 
-      <hr id="hr1"></hr>
-      <p className="markscontent"> {fetchMarksData}</p> 
-    </div>
-    </div>
-    <br/>
-    
-</div> 
+    <div className="view-feedback">
+  <br/>
+      <h1 id="caption">My Marks</h1>
+      <br/><br/>
+      <div className="card">
+      <div className="container">
+        <h4 id="marks-topic"><b>Milestone 1 </b></h4> 
+        <hr id="hr1"></hr>
+        <p className="markscontent"> {fetchMarksData}</p> 
+      </div>
+      </div>
+      <br/>
+      <div className="card">
+      <div className="container">
+        <h4 id="marks-topic"><b>Milestone 2 </b></h4> 
+        <hr id="hr1"></hr>
+        <p className="markscontent"> Not available</p> 
+      </div>
+      </div>
+      <br/>
+      <div className="card">
+      <div className="container">
+        <h4 id="marks-topic"><b>Milestone 3 </b></h4> 
+        <hr id="hr1"></hr>
+        <p className="markscontent"> Not available</p> 
+      </div>
+      </div>
+      <br/><br/>
+      <div className="card">
+      <div className="container">
+        <h4 id="marks-topic"><b>Milestone 4 </b></h4> 
+        <hr id="hr1"></hr>
+        <p className="markscontent"> Not available</p> 
+      </div>
+     
+      </div>
+
+
+      <br/>
+      
+</div>
     
     
     
