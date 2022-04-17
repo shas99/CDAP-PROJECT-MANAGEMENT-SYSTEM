779b50cf826463a25c1a38bde13e36c0dddc6266
Chathushka Rodrigo <72795289+ChathushkaRodrigo@users.noreply.github.com>
2022-04-17 20:40:51 +0530

    Refined code on LoginScreen and added gitignore for eslintcache

--- client/src/components/screens/MatchedSupervisors.js
+++ client/src/components/screens/MatchedSupervisors.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "./MatchedSupervisors.css";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 
 const MatchedSupervisors = ({history}) => {
