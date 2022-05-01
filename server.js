6b2d1500665184e3cc31165b2b76a56fea4ac6f7
buddhi549 <humbleandwise16@gmail.com>
2022-05-01 18:01:18 +0530

    installed nexmo and nunjucks for 2FA

--- server.js
+++ server.js
@@ -2,12 +2,25 @@ require('dotenv').config({path: "./config.env"})
 const express = require('express');
 const connectDB = require('./config/db')
 const errorHandler = require('./middleware/error')
+const bodyParser = require('body-parser')
+const nunjucks=require('nunjucks')
+const Nexmo = require('nexmo')
+
 
 connectDB();
 
 const app = express();
 
 app.use(express.json())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+nunjucks.configure('views',{express:app})
+
+const nexmo = new Nexmo({
+    apiKey:'1d0d5bcd',
+    apiSecret:'NiSs1FWkyG1tp72S'
+})
 
 // ... other imports 
 const path = require("path")
