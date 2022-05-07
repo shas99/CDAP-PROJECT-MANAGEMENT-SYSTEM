78821e40c6304b2d5443e95b75b0ecb3b4f88372
buddhi549 <humbleandwise16@gmail.com>
2022-05-07 11:29:28 +0530

    Created projectbidlist routes

---
+++ routes/projectbidlist.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { route } = require('express/lib/application');
+const router = express.Router()
+const projectbidlistcontroller = require('../controllers/projectbidlistcontroller');
+
+router.get('/projectbidlist/:id',projectbidlistcontroller.get_projectbidlist_projects);
+router.post('/projectbidlist/:id',projectbidlistcontroller.add_projectbidlist_project);
+router.delete('/projectbidlist/:groupId/:projectId',projectbidlistcontroller.delete_project);
+
+module.exports = router
