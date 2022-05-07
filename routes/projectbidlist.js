585d8e8f7fa3ef7be9d90ea8254aa77e45069fb2
buddhi549 <humbleandwise16@gmail.com>
2022-05-07 11:44:55 +0530

    Created Project schema

--- routes/projectbidlist.js
+++ routes/projectbidlist.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { route } = require('express/lib/application');
 const router = express.Router()
-const projectbidlistcontroller = require('../controllers/projectbidlistcontroller');
+const projectbidlistcontroller = require('../controllers/projectbidlistcontroller.js');
 
 router.get('/projectbidlist/:id',projectbidlistcontroller.get_projectbidlist_projects);
 router.post('/projectbidlist/:id',projectbidlistcontroller.add_projectbidlist_project);
-router.delete('/projectbidlist/:groupId/:projectId',projectbidlistcontroller.delete_project);
+router.delete('/projectbidlist/:groupID/:projectId',projectbidlistcontroller.delete_project);
 
 module.exports = router;
