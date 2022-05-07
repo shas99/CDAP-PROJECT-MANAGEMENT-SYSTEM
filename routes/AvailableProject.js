13c07d9a6960d6449c2b71c19d7b7bdacae0eb5f
Chathushka Rodrigo <72795289+ChathushkaRodrigo@users.noreply.github.com>
2022-05-07 16:32:29 +0530

    Added route path for bidding-create

--- routes/AvailableProject.js
+++ routes/AvailableProject.js
@@ -2,12 +2,11 @@ const express = require('express');
 const { route } = require('express/lib/application');
 const router = express.Router()
 
-const {viewAvailableProjects,viewspecificproject} = require('../controllers/AvailableProject')
+const {viewAvailableProjects,viewspecificproject,placeBidonAvailableProject} = require('../controllers/AvailableProject')
 
 
 router.route("/availableProjects").get(viewAvailableProjects) //router for View Available Projects
-
-
-router.route("/availableProjects/:id").get(viewspecificproject)
+router.route("/availableProjects/:id").get(viewspecificproject)  //router for viewing specific project
+router.route("/availableProjects/placeBidding/:id").post(placeBidonAvailableProject) //place bidding router
 
 module.exports = router
