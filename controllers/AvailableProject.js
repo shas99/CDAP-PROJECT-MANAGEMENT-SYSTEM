1232b47976182b9ed4764a73410ecafd9bac79bb
buddhi549 <humbleandwise16@gmail.com>
2022-05-07 23:54:58 +0530

    implemented backend for increase project bid count

--- controllers/AvailableProject.js
+++ controllers/AvailableProject.js
@@ -47,3 +47,69 @@ exports.viewspecificproject = async(req,res,next) => {
     }
 }
 
+//final increase count
+exports.increasebidcount = async(req,res,next) => {
+    try{
+        const availableprojectid = req.body;
+        const projectcount = await AvailableProject.findById(availableprojectid)
+        console.log(projectcount);
+        projectcount.projectBiddingCount = projectcount.projectBiddingCount + 1; 
+        await projectcount.save();
+        
+        
+
+    }catch(error){
+        res.status(500).json({success:false, error:error.message})
+
+    }
+}
+
+//Increase project count
+module.exports.add_projectbidlist_project = async (req,res) => {
+    const userId = req.params.id;
+    const { projectBiddingCount } = req.body;
+
+    try{
+        let projectbidlist = await Projectbidlist.findOne({groupID});
+        let project = await Project.findOne({_id: projectID});
+        if(!project){
+            res.status(404).send('Project not found!')
+        }
+     
+        const name = project.name;
+        
+        if(projectbidlist){
+            // if projectbidlist exists for the group
+            let projectIndex = projectbidlist.projects.findIndex(p => p.projectID == projectID);
+
+            // Check if project exists or not
+            if(projectIndex > -1)
+            {
+                let projectProject = projectbidlist.projects[projectIndex];
+                projectProject.quantity += quantity;
+                projectbidlist.projects[projectIndex] = projectProject;
+            }
+            else {
+                projectbidlist.projects.push({ projectID, name, quantity});
+            }
+            
+            projectbidlist = await projectbidlist.save();
+            return res.status(201).send(projectbidlist);
+        }
+        else{
+            // no projectbidlist exists, create one
+            const newProjectbidlist = await Projectbidlist.create({
+                projectID,
+                projects: [{ projectID, name, quantity }],
+                
+            });
+            return res.status(201).send(newProjectbidlist);
+        }       
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).send("Something went wrong");
+    }
+}
+
+
