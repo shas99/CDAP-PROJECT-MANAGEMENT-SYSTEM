389095d72e8267ef6bacc36efd11b37da5b6aa84
buddhi549 <humbleandwise16@gmail.com>
2022-05-08 16:28:58 +0530

    wrote alternate code for viewavailable projects

--- client/src/components/screens/ViewAvailableProjects.js
+++ client/src/components/screens/ViewAvailableProjects.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+/*import { useState, useEffect } from "react";
 import axios from "axios";
 import "./PrivateScreen.css"
 import "./ViewAvailableProjects.css"
@@ -54,13 +54,129 @@ export default class ViewAvailableProjects extends Component {
     } = this.retrieveAvailableProjects;
 
     return (
-   /* <div>
-      {this.retrieveAvailableProjects}
-    </div>*/
+  
     <div>
       <h4>{projectName}</h4>
 
     </div>
     );
   }
-}
+}*/
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./ViewAvailableProjects.css";
+import { Link } from "react-router-dom";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
+const ViewAvailableProjects = ({history}) => {
+    const [error, setError] = useState("");
+    const [privateData, setPrivateData] = useState("");
+    // const [fetchGroupData, setGroupData] = useState("")
+    const [projects,setprojects] = useState("")
+    
+    useEffect(() => {
+      const fetchPrivateDate = async () => {
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+          },
+        };
+  
+        try {
+          const { data} = await axios.get("/api/private", config);
+          
+          setPrivateData(data.data);
+        } catch (error) {
+          localStorage.removeItem("authToken");
+          setError("You are not authorized please login");
+        }
+      };
+  
+
+      const fetchprojects = async () => {
+        const projectsconfig = {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+          },
+        };
+  
+        try {
+          const { data} = await axios.get("/api/AvailableProject/availableprojects",projectsconfig);
+          const projArray = data.data.split("/")
+          const projectsArray = projArray[1].split(",")
+          // console.log(suggArray)
+          projectsArray.map((a) => console.log(a))
+          setprojects(projectsArray);
+        } catch (error) {
+          console.log(error)
+          // setError("Oops couldn't retreive suggestions");//fix this
+        }
+      };
+    //   fetchGroupData()
+      fetchprojects()
+      fetchPrivateDate();
+    }, [history]);
+  
+    //Logout feature
+    const logOutHandler=()=>{
+      localStorage.removeItem("authToken");
+      history.push("/login");
+  
+    };
+
+    const listHandler=()=>{
+      try{
+
+        const lists = projects.map((n)=>
+        <li>{n}</li>)
+        return(
+          <ul>{lists}</ul>
+        )
+      }catch(e){
+        console.error(e)
+      }
+      // <ul>
+      // {suggestions.map((m) => <li>{m}</li>)}
+      // </ul>
+  
+    };
+
+
+  
+    return  error ? ( 
+  
+        <span className="error-message">{error}</span>
+      ) : ( 
+    
+        <>
+        <div id="back">
+        <Header/>
+        <p style={{color:"#FFF",textAlign:"right"}}>
+        {privateData}  
+        &nbsp;&nbsp;&nbsp;&nbsp;
+       
+        <button onClick={logOutHandler} id="logout">Log Out</button>
+          </p>
+          <div id="card">
+
+          <p style={{color:"#FFF"}}>
+          
+          
+          <h1 id="caption">All projects</h1>
+          <hr id="hr"></hr>
+          <p id="List">
+          {listHandler()}
+          </p>
+          </p>
+          </div>
+        <Footer/>
+        </div>
+        </>
+      );
+    };
+    
+    export default ViewAvailableProjects;
