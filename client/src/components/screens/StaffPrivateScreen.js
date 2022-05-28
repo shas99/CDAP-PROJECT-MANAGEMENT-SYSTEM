254fe97f108310bc0d6695e6b514f53c834e5fc3
buddhi549 <humbleandwise16@gmail.com>
2022-05-28 11:46:23 +0530

    configured zeplin,created an image folder and did initial styling

--- client/src/components/screens/StaffPrivateScreen.js
+++ client/src/components/screens/StaffPrivateScreen.js
@@ -57,8 +57,10 @@ const StaffPrivateScreen = ({history}) => {
     <button onClick={logOutHandler} id="logout">Log Out</button>
       </p>
       
-
+    <div className="Rectangle-43">
+    <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
       <Link to= "#" id="Regs"><button className="buttons" onClick="#">Add Feedback</button></Link>
+      </div>
         <br/>
         <Link to="#" id="Regs"><button className="buttons" onClick="#">Add Marks</button></Link>
         
@@ -67,10 +69,19 @@ const StaffPrivateScreen = ({history}) => {
           <Link to="/staffreport" id="Regs"><button className="buttons" onClick="/staffreport ">View Report</button></Link>
 
           <Link to="/staffrecommendationform" id="Regs"><button className="buttons" onClick="/staffrecommendationform ">Staff</button></Link>
-      
+      <div className="Rectangle-43">
+        <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
           <Link to="/staffproject" id="Regs"><button className="buttons" onClick="/staffproject ">View projects</button></Link>
         
-  
+          </div>{/* <img src="img/fa-solid_user-cog.png"
+                  srcset="img/group-114@2x.png 2x,
+                  img/group-114@3x.png 3x"
+                  class="Group-114"/> */}
+
+       
+        
+
+                  
   
     <Footer/>
 
