b7299ab97deb4c523f428b579dcd8003997a4960
buddhi549 <humbleandwise16@gmail.com>
2022-06-19 11:20:17 +0530

    added addmarks route

--- routes/staffPrivate.js
+++ routes/staffPrivate.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getPrivateData } = require("../controllers/staffPrivate");
+const { getPrivateData, addmarks } = require("../controllers/staffPrivate");
 const { protect } = require("../middleware/staffAuth");
 
 
-
+//add marks route
+router.route("/staffPrivate").get(protect,addmarks)
 router.route("/staffPrivate").get(protect, getPrivateData);
 
 
