47b718cc86d53a74b627cc6b5433e7639222c134
buddhi549 <humbleandwise16@gmail.com>
2022-06-20 15:31:55 +0530

    finished creating the enter marks form

--- client/src/App.js
+++ client/src/App.js
@@ -26,6 +26,7 @@ import StaffLoginScreen from "./components/screens/StaffLoginScreen";
 import StaffRegisterScreen from "./components/screens/StaffRegisterScreen";
 import StaffPrivateScreen from "./components/screens/StaffPrivateScreen";
 import StaffRecommendationForm from "./components/screens/StaffRecommendationForm";//Staff Recommendation Form
+import EnterMarks from "./components/screens/EnterMarks";
 
 import ViewAvailableProjects from "./components/screens/ViewAvailableProjects";
 import ProjectBidding from "./components/screens/ProjectBidding";
@@ -78,6 +79,8 @@ const App = () => {
          <Route exact path="/staffRegister" component={StaffRegisterScreen}/>
 
           <Route exact path="/staffPrivate" component={StaffPrivateScreen}/> 
+
+          <Route exact path="/addmarks" component={EnterMarks}/>
 
 
          <Route exact path="/viewavailableprojects" component={ViewAvailableProjects}/>
