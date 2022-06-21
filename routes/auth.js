39fffac473cfdcf4cb7d71debcf7a340c86e34f3
Chathushka Rodrigo <72795289+ChathushkaRodrigo@users.noreply.github.com>
2022-06-21 16:21:44 +0530

    Updated 01

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,37 @@
+// const express = require('express');
+// const { route } = require('express/lib/application');
+// const router = express.Router()
+// const { register,login,forgotpassword,resetpassword} = require('../controllers/auth')
+
+
+// router.route("/register").post(register)
+
+// router.route("/login").get(login)
+
+// router.route("/forgotpassword").post(forgotpassword)
+
+// router.route("/resetpassword/:resetToken").put(resetpassword)
+
+// // router.route("/OTP").put(OTP)
+
+
+// module.exports = router
+
+
+  
 const express = require('express');
 const { route } = require('express/lib/application');
 const router = express.Router()
-const { register,login,forgotpassword,resetpassword,OTP} = require('../controllers/auth')
+const { register,login,forgotpassword,resetpassword} = require('../controllers/auth')
 
 
 router.route("/register").post(register)
 
-router.route("/login").put(login)
+router.route("/login").post(login)
 
 router.route("/forgotpassword").post(forgotpassword)
 
 router.route("/resetpassword/:resetToken").put(resetpassword)
-
-router.route("/OTP").put(OTP)
 
 
 module.exports = router
