2f4bcf6a37f851917955668cf8f1d0c06f283c50
Chathushka Rodrigo <72795289+ChathushkaRodrigo@users.noreply.github.com>
2022-06-24 23:21:25 +0530

    Implemented Proposal Report Marking Updated and Retrieve API

--- routes/MarkingRubrik.js
+++ routes/MarkingRubrik.js
@@ -2,12 +2,16 @@ const express = require('express');
 const { route } = require('express/lib/application');
 const router = express.Router()
 
-const {proposalMarkingConfiguration,viewProposalMarkingDetails} = require('../controllers/MarkingRubrik')
+const {proposalMarkingConfiguration,viewProposalMarkingDetails,proposalReportMarkingConfiguration,viewProposalReportMarkingDetails} = require('../controllers/MarkingRubrik')
 
-//export the api
 
+//Proposal Presentation Routes
 router.route("/proposalMarkingConfiguration/update/:id").put(proposalMarkingConfiguration)
 router.route("/proposalMarkingConfiguration/:id").get(viewProposalMarkingDetails)
+
+//Proposal Report Routes
+router.route("/proposalReportMarkingConfiguration/update/:id").put(proposalReportMarkingConfiguration)
+router.route("/proposalReportMarkingConfiguration/:id").get(viewProposalReportMarkingDetails)
 
 module.exports = router
 
