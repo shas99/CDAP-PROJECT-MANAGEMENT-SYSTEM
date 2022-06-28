b577140503443e128c73973ba0aad3bbd2713bc4
Chathushka Rodrigo <72795289+ChathushkaRodrigo@users.noreply.github.com>
2022-06-28 11:20:40 +0530

    Implemented Status Document Backend API - Update and Retrieve

--- routes/MarkingRubrik.js
+++ routes/MarkingRubrik.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { route } = require('express/lib/application');
 const router = express.Router()
 
-const {proposalMarkingConfiguration,viewProposalMarkingDetails,proposalReportMarkingConfiguration,viewProposalReportMarkingDetails} = require('../controllers/MarkingRubrik')
+const {proposalMarkingConfiguration,viewProposalMarkingDetails,proposalReportMarkingConfiguration,viewProposalReportMarkingDetails,statusDocumentMarkingConfiguration,viewStatusDocumentMarkingDetails} = require('../controllers/MarkingRubrik')
 
 
 //Proposal Presentation Routes
@@ -12,6 +12,11 @@ router.route("/proposalMarkingConfiguration/:id").get(viewProposalMarkingDetails
 //Proposal Report Routes
 router.route("/proposalReportMarkingConfiguration/update/:id").put(proposalReportMarkingConfiguration)
 router.route("/proposalReportMarkingConfiguration/:id").get(viewProposalReportMarkingDetails)
+
+//Status Document 01 Routes
+router.route("/statusDocumentMarkingConfiguration/update/:id").put(statusDocumentMarkingConfiguration)
+router.route("/statusDocumentMarkingConfiguration/:id").get(viewStatusDocumentMarkingDetails)
+
 
 module.exports = router
 
