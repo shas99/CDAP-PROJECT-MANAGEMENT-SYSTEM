e72268b637a8be1e7e71ff721e5da1f5fc02c1cc
buddhi549 <humbleandwise16@gmail.com>
2022-06-30 11:40:01 +0530

    created add status document 1 marks method in controllers

--- controllers/staffPrivate.js
+++ controllers/staffPrivate.js
@@ -2,6 +2,8 @@ const Staff = require('../models/Staff')
 const jwt = require("jsonwebtoken");
 const entermarks = require('../models/Marks');
 
+const enterstatusdocument1marks = require('../models/StatusDocument1Marks');
+
 const enterproposalreportmarks = require('../models/ProposalReportMarks');
 exports.getPrivateData = async (req,res,next) => {
     let token//to retreive username in backend
@@ -52,6 +54,45 @@ exports.addproposalreportmarks = async(req,res,next) => {
     try{
         const proposalreportmarksadd = await enterproposalreportmarks.create({
             groupID,studentIDs,studentnames,cosupervisor,supervisor,extrafeedback,provengapmarks1,provengapmarks2,capabilitymarks1,capabilitymarks2,implementationmarks1,implementationmarks2,implementationmarks3,communicationmarks1,communicationmarks2,communicationmarks3,commercializationmarks1
+        })
+        res.status(210).json({
+            success: true,
+            data: "Added marks successfully"
+        })
+    }catch(error){
+        next(error)
+    }
+}
+
+//Add proposal report marks method
+
+exports.addproposalreportmarks = async(req,res,next) => {
+    const {groupID,studentIDs,studentnames,cosupervisor,supervisor,extrafeedback,provengapmarks1,provengapmarks2,capabilitymarks1,capabilitymarks2,implementationmarks1,implementationmarks2,implementationmarks3,communicationmarks1,communicationmarks2,communicationmarks3,commercializationmarks1 } = req.body
+
+    console.log("Error finding" + groupID)
+    try{
+        const proposalreportmarksadd = await enterproposalreportmarks.create({
+            groupID,studentIDs,studentnames,cosupervisor,supervisor,extrafeedback,provengapmarks1,provengapmarks2,capabilitymarks1,capabilitymarks2,implementationmarks1,implementationmarks2,implementationmarks3,communicationmarks1,communicationmarks2,communicationmarks3,commercializationmarks1
+        })
+        res.status(210).json({
+            success: true,
+            data: "Added marks successfully"
+        })
+    }catch(error){
+        next(error)
+    }
+}
+
+
+//Add status document 1  marks method
+
+exports.addstatusdocument1marks = async(req,res,next) => {
+    const {projectNo,studentIDs,ganttchartmarks,actualtimemarks,breakdownmarks,managementtoolmarks, ganttchartremarks,actualtimeremarks,breakdownremarks,managementtoolremarks,supervisor,cosupervisor } = req.body
+
+    console.log("Error finding" + projectNo)
+    try{
+        const statusdocument1marksadd = await enterstatusdocument1marks.create({
+            projectNo,studentIDs,ganttchartmarks,actualtimemarks,breakdownmarks,managementtoolmarks, ganttchartremarks,actualtimeremarks,breakdownremarks,managementtoolremarks,supervisor,cosupervisor
         })
         res.status(210).json({
             success: true,
