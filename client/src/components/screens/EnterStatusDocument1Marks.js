71748e6a67ee5379429c1fa3fcd1d3dc683e503e
buddhi549 <humbleandwise16@gmail.com>
2022-06-30 12:08:57 +0530

    initial methods in EnterStatusDocument1Marks.js

---
+++ client/src/components/screens/EnterStatusDocument1Marks.js
@@ -0,0 +1,68 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+// import "./MatchedSupervisors.css";
+import "./EnterStatusDocument1Marks.css";
+import Header from "../Header/Header";
+
+const EnterStatusDocument1Marks = ({history}) => {
+    const [error,setError]= useState("");
+    const [privateData,setPrivateData]= useState("");
+    const [projectNo, setprojectNo ] = useState("");
+    const [ganttchartmarks, setganttchartmarks] = useState("");
+    const [actualtimemarks, setactualtimemarks] = useState("");
+    const [breakdownmarks, setbreakdownmarks] = useState("");
+    const [managementtoolmarks, setmanagementtoolmarks] = useState("");
+    const [ganttchartremarks, setganttchartremarks] = useState("");
+    const [actualtimeremarks, setactualtimeremarks] = useState("");
+    const [breakdownremarks, setbreakdownremarks] = useState("");
+    const [managementtoolremarks, setmanagementtoolremarks] = useState("");
+    const [supervisor, setsupervisor] = useState("");
+    const [cosupervisor, setcosupervisor] = useState("");
+    const [enterstatusdocument1marks,setenterstatusdocument1marks] = useState("")
+    const [fetchenterstatusdocument1marksData, setenterstatusdocument1marksData] = useState("");
+
+    useEffect(() => {
+        const fetchenterstatusdocument1marksData= async () => {
+            const enterstatusdocument1marksconfig = {
+                headers: {
+                    "Content-Type":"application/json",
+                    Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+                }
+            }
+            try{
+                const { data} = await axios.get("/api/staffPrivate/addstatusdocument1marks", enterstatusdocument1marksconfig);
+                const enterstatusdocument1marksArray = data.data.split("/")
+                console.log(enterstatusdocument1marksArray[0])
+                const enterstatusdocument1marks =enterstatusdocument1marksArray[0].split(",")
+                setenterstatusdocument1marks(enterstatusdocument1marks)
+                setenterstatusdocument1marksData(enterstatusdocument1marksArray[0]);
+               
+            }catch(error){
+                
+            }
+        }
+
+        const fetchPrivateDate = async () => {
+            const config = {
+                headers:{
+                    "Content-Type":"application/json",
+                    Authorization: `Bearer ${localStorage.getItem("authToken")}`
+                },
+            }
+            try{
+                const { data } = await axios.get("/api/staffPrivate/staffPrivate", config); //change to resolve logged out error
+                setPrivateData(data.data);
+            }catch(error){
+                localStorage.removeItem("authToken");
+                setError("You are not authorized please login");
+            }
+
+        }
+
+        //fetch Marks Data()
+
+        fetchPrivateDate();
+        fetchenterstatusdocument1marksData()
+    }, [history])
+
+}
