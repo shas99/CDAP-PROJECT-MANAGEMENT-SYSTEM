b868c57c768c2c536d4d44704b06de49aef764ab
buddhi549 <humbleandwise16@gmail.com>
2022-07-09 13:07:20 +0400

    finished coding propsalpresentalmarks.js file

--- client/src/components/screens/ViewProposalPresentationMarks.js
+++ client/src/components/screens/ViewProposalPresentationMarks.js
@@ -49,4 +49,35 @@ const ViewProposalPresentationMarks = ({history}) => {
         fetchPrivateDate();
     }, [history]);
 
+    //logout feature
+    const logOutHandler=()=>{
+        localStorage.removeItem("authToken");
+        history.push("/login");
+    };
+
+    return error ? (
+        <span className="error-message">{error}</span>
+    ) : (
+        <>
+        <div id = "back">
+            <Header/>
+            <p style={{color:"#FFF",textAlign:"right"}}>
+                {privateData}
+                &nbsp;&nbsp;&nbsp;&nbsp;
+
+                <button onClick={logOutHandler} id="logout">Log out</button>
+            </p>
+
+            <p style={{color:"#FFF"}}>
+                <br/><br/><br/><br/>
+                <h1>Your Proposal Presentation marks are</h1><br/>
+                {fetchProposalPresentationMarksData}<br/><br/><br/><br/>
+                
+            </p>
+            <Footer/>
+        </div>
+        </>
+    )
+
 }
+export default ViewProposalPresentationMarks;
