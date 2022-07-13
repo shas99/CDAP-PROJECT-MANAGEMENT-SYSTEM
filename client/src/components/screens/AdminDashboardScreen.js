7f45ab5aea12123044d1aa18a6909e9bcd7955ef
Chathushka Rodrigo <72795289+ChathushkaRodrigo@users.noreply.github.com>
2022-07-13 15:09:06 +0530

    Resolved minor conflicts

--- client/src/components/screens/AdminDashboardScreen.js
+++ client/src/components/screens/AdminDashboardScreen.js
@@ -49,7 +49,7 @@ const AdminDashboard = ({history}) => {
     <span className="error-message">{error}</span>
   ) : ( 
 
-    <>
+<>
     <div id="back">
 
 <Header/>
@@ -97,9 +97,9 @@ const AdminDashboard = ({history}) => {
 
 
  
-<Footer/>
+  <Footer/>
 
-</div>
+
     </>
   );
 };
