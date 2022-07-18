d08c507eb4b6d80198e84cf5e1046a11030a4865
buddhi549 <humbleandwise16@gmail.com>
2022-07-18 20:53:05 +0400

    created a route for view proposal report marks

--- routes/ViewMarks.js
+++ routes/ViewMarks.js
@@ -2,8 +2,10 @@ const express = require('express');
 const { route } = require('express/lib/application');
 const router = express.Router()
 
-const {viewproposalpresentationmarks} = require('../controllers/viewmarks')
+const {viewproposalpresentationmarks,viewproposalreportmarks} = require('../controllers/viewmarks')
 
 router.route("/viewproposalpresentationmarks").get(viewproposalpresentationmarks)
+
+router.route("/viewproposalreportmarks").get(viewproposalreportmarks)
 
 module.exports = router
