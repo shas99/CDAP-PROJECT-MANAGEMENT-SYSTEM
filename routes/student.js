49233bf6531747f91bd4337e208162ffea870928
shas99 <pshasvathan1999@gmail.com>
2022-07-22 21:10:39 +0530

    git merge error fixed

--- routes/student.js
+++ routes/student.js
@@ -2,11 +2,10 @@ const express = require('express');
 const { route } = require('express/lib/application');
 const router = express.Router()
 
-<<<<<<< HEAD
-const {viewfeedback,viewmarks, viewAvailableGroups, StudentTopicInterestingForm,userprofilemanagement,edituserprofile,viewimage} = require('../controllers/student')
-=======
-const {viewfeedback,viewmarks, viewAvailableGroups, StudentTopicInterestingForm,userprofilemanagement,edituserprofile,status,retrieveData} = require('../controllers/student')
->>>>>>> 361ece6fb7f52cde2b1cfa2789d30101f9f19c76
+
+const {viewfeedback,viewmarks, viewAvailableGroups, StudentTopicInterestingForm,userprofilemanagement,edituserprofile,viewimage,status,retrieveData} = require('../controllers/student')
+
+
 
 
 router.route("/viewfeedback").get(viewfeedback)//to view feedback
@@ -19,11 +18,11 @@ router.route("/userprofilemanagement").get(userprofilemanagement)//student profi
 
 router.route("/edituserprofile").put(edituserprofile)//student - edit profile feature
 
-<<<<<<< HEAD
-router.route("/viewimage").get(viewimage)
-=======
+
+// router.route("/viewimage").get(viewimage)
+
 router.route("/status").get(status)
->>>>>>> 361ece6fb7f52cde2b1cfa2789d30101f9f19c76
+
 
 router.route("/retrieveData").get(retrieveData)
 
