2846ae9371346473c870211350d557a919eb1c88
pasinduVinod <pasindu.vinod1999@gmail.com>
2022-07-25 13:00:25 +0530

    Staff Dashboard styled 1

--- client/src/components/screens/StaffDashboardNew.js
+++ client/src/components/screens/StaffDashboardNew.js
@@ -278,7 +278,7 @@ export default function StaffDashboard ({history}) {
 
                       
 
-                       <div class=" w-36 gap-1  h-12 text-sm rounded-lg flex justify-center items-center shadow-md bg-blue-700 lg:bg-gray-800 hover:bg-blue-700 duration-300 gap-x-100">
+                       <div class="w-36 gap-1  h-12 text-sm rounded-lg flex justify-center items-center shadow-md bg-blue-700 lg:bg-gray-800 hover:bg-blue-700 duration-300 gap-x-100">
 
                             <button>
                                 <a href='#'>Add Feedback</a>
