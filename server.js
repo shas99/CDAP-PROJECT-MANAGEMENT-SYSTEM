f65ac5fa2d8eb03ef03c4c754227d8586fb3d14c
Shasvathan <pshasvathan1999@gmail.com>
2022-08-12 15:19:48 +0530

    deployment issue fix attempt 6

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // Set EJS as templating engine
-app.set("view engine", "ejs");
+// app.set("view engine", "ejs");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
