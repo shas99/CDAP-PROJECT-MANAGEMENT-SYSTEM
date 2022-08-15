e3ad0c1e384734b755cf2a1a123e9104759ab846
chathushkarodrigo <chathushkarodrigo@gmail.com>
2022-08-15 18:18:23 +0530

    Added Tables for Supervisor and Project selection

--- client/src/components/screens/MatchedSupervisors.js
+++ client/src/components/screens/MatchedSupervisors.js
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import { render } from "@testing-library/react";
+import SideNavigationBar from "../SideNavigationBar/sideNavigationBarComponent";
 
 const MatchedSupervisors = ({history}) => {
     const [error, setError] = useState("");
@@ -120,75 +121,7 @@ const MatchedSupervisors = ({history}) => {
 
 
      
-      // const fetchsuggestions = async () => {
-      //   const suggestconfig = {
-      //     headers: {
-      //       "Content-Type": "application/json",
-      //       Authorization: `Bearer ${localStorage.getItem("authToken")}`,
-      //     },
-      //   };
   
-      //   try {
-      //     const { data} = await axios.get("/api/group/group",suggestconfig);
-      //     const sugArray = data.data.split("/")
-      //     const suggArray = sugArray[1].split(",")
-      //     console.log(suggArray)
-      //     // console.log(suggArray)
-      //     suggArray.map((a) => console.log(a))
-      //     setsuggestions(suggArray);
-      //   } catch (error) {
-      //     console.log(error)
-      //     // setError("Oops couldn't retreive suggestions");//fix this
-      //   }
-      // };
-    //   fetchGroupData()
-      // fetchsuggestions()
-
-    // const fetchProjectDescription = async ()=>{
-    //     const PID = ProjectID;
-    //     console.log(PID)
-    //     const projectConfigto = {
-    //       headers: {
-    //         "Content-Type": "application/json",
-    //         Authorization: `Bearer ${localStorage.getItem("authToken")}`,
-    //       },
-    //     };
-     
-    //     try{
-    //       const{data} = await axios.get("/api/AvailableProject/availableProjects/${PID}",projectConfigto);
-    //       const description = data.projectDescription
-    //       console.log(description)
-    //       setProjectDescription(description)
-
-         
-    //     }catch(error){
-          
-          
-    //     }
-        
-  
-    // }
-
-    // const fetchhandleSubmit  = async () => {
-    //   const subconfig = {
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //       Authorization: `Bearer ${localStorage.getItem("authToken")}`,
-    //     },
-    //   };
-
-
-    //   const ab = localStorage.getItem("authToken")
-    //   try {
-    //     const {data} =  await axios.post("/api/group/showSupervisors",{params: {ab}}, subconfig); //need to pass the batch id as a parameter to backend
-    //     const array = Object.entries(data.data)
-
-    //     setSupervisorName(array);
-    //     //console.log(array)
-        
-    //   }
-    //   catch(error){}
-    // };
 
 
 
@@ -196,16 +129,11 @@ const MatchedSupervisors = ({history}) => {
       fetchPrivateDate()
       fetchAvailableSupervisors()
       fetchAvailableProjects()
-      // fetchProjectDescription()
-      // fetchSupervisorBiddingDetails()
+     
      
     }, [history]);
 
-    // const updateSupervisors = e =>{
-    //     let newarr = [...SelectedSupervisors];
-    //     newarr.push(e.target.value)
-    //     setSelectedSupervisors(newarr);
-    // } 
+   
     
     
     // Submit form
@@ -253,29 +181,7 @@ const MatchedSupervisors = ({history}) => {
   const removesupervisor = (e) => { setSelectedSupervisors([...SelectedSupervisors.filter((supervisor) =>
      supervisor !== e.target.value)]) }
   
-    //Logout feature
-    // const logOutHandler=()=>{
-    //   localStorage.removeItem("authToken");
-    //   history.push("/login");
-  
-    // };
-
-    // const listHandler=()=>{
-    //   try{
-
-    //     const lists = suggestions.map((n)=>
-    //     <li>{n}</li>)
-    //     return(
-    //       <ul>{lists}</ul>
-    //     )
-    //   }catch(e){
-    //     console.error(e)
-    //   }
-    //   // <ul>
-    //   // {suggestions.map((m) => <li>{m}</li>)}
-    //   // </ul>
-  
-    // };
+   
 
     
 
@@ -286,150 +192,283 @@ const MatchedSupervisors = ({history}) => {
       ) : ( 
     
         <>
-        <div id="back">
+        <div className="bg-gray-700 h-[75rem]">
         <Header/>
-        {/* <p style={{color:"#FFF",textAlign:"right"}}> */}
-        {/* {privateData}   */}
-        {/* &nbsp;&nbsp;&nbsp;&nbsp; */}
-       
-        {/* <button onClick={logOutHandler} id="logout">Log Out</button> */}
-          {/* </p> */}
-          {/* <div id="card"> */}
-
-          {/* <p style={{color:"#FFF"}}> */}
-          
-          
-          {/* <h1 id="caption">Your Supervisor suggestions are</h1>
-          <hr id="hr"></hr>
-          <p id="List">
-          {listHandler()}
-          </p> */}
-
-
-
-          {/* </p> */}
-          {/* </div> */}
-
-
-
-          {/* US-01 Pasindu Vinod*/}
-          
-          <div style={{color:"white",width:"100%"}}>
-            <p className="text-center text-2xl font-semibold ml-10 mt-5">Place Bids for Supervisors {batchID}</p>
-            <br/>
-            <div id="Supervisors" style={{display:"in"}}>
-              <div>
-                <caption className="text-center mt-5">
-                  Select supervisor
-                </caption>
-              <form onSubmit={SubmitBidding}>
-              <table id = "stable">
-                
-                 
-                  {SupervisorName.map (supervisor => 
-                  <tr>
-                    <td><input type="checkbox" value={supervisor[1][0]} name="supervisor" onChange={(e) => {updateSupervisors(e)}}></input></td>
-                    <td>{supervisor[1][1]}</td>
-                    <td>{supervisor[1][2]}/{batchType}</td>
-                  </tr>
-                  )}
-                
-            </table>
-            {/* {console.log("Selected supervisors"+SelectedSupervisors)} */}
-           
-
-           
-           
-              <h3 className="ml-15 mt-5">
-              Select Project
-              </h3>
-              <table style={{width:"700px"}}>
-              {/* <select id = "Project" name="Project" style={{color:"black"}} onChange={e => {setProjectID(e.target.value)}}> */}
-                {/* <option>Select a Project</option> */}
-
-                <tr>
-                  <th style={{width:"8rem"}}>Project Category </th>
-                  <th style={{width:"rem"}}>Project Name</th>
-                  <th style={{width:"6rem"}}>Project Description</th>
-                  <th style={{width:"6rem"}}></th>
-
-                </tr>
-                
-                {/* {Projects.map (project => 
-                <tr>
-                  <td>{project[1].projectType}</td>
-                  <td>{project[1].projectName}</td>
-                  <td>{project[1].projectDescription}</td>
-                  <td>
-                    
-                    <input type="radio" value={project[1]._id} name="project"></input>
-
-                  </td>
-                </tr>
-                  
-                )} */}
-
-                {Projects.map (project =>{  
-                  if(project[1].projectStatus == false){
-                    return(
-                    <tr>
-                      <td>{project[1].projectType}</td>
-                      <td>{project[1].projectName}</td>
-                      <td>{project[1].projectDescription}</td>
-                      <td>
-                        <input type="radio" name="project" value={project[1]._id} onChange={(e) => setSelectedProject(e.target.value)}/> 
-                      </td>
-                    </tr>
-                    )
-                  }else{
-                    return(
-                    <tr>
-                      <td>{project[1].projectType}</td>
-                      <td>{project[1].projectName}</td>
-                      <td>{project[1].projectDescription}</td>
-                      <td>
-                        Taken 
-                      </td>
-                    </tr>
-                    )
-                  }                          
-                               
-                  })}
-                
-
-
-              {/* </select>  */}
-              </table>
-
-              {/* <input type="submit">Place Bid</input> */}
-              <button type="submit">Place Bid</button>
-
-              {/* <button onClick={e => {}}>Find Project</button> */}
-
-              {/* <button onClick={() => setVisible(true)}>Show</button>
-      {visible && <div><select></select></div>} */}
-
-
-              {/* <select id = "Project" name="Project" style={{color:"black"}} onChange={e => {setProjectDescription(e.target.value)}}>
-                
-                {Projects.map (projecta => 
-                   <option>{projecta[1].projectName}</option>
-                )}
-              </select>  */}
-
-
+        <SideNavigationBar page="MatchedSupervisors"/>
+        {/* Table for Supervisor select */}
+         
+<div class="overflow-x-auto shadow-md sm:rounded-lg w-[50rem] ml-[20rem] mt-[-50rem]">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="py-3 px-6">
+                    Supervisor name
+                </th>
+                <th scope="col" class="py-3 px-6">
+                    Taken Projects
+                </th>
+                <th scope="col" class="py-3 px-6">
+                    Select
+                </th>
+               
               
-            </form>
-            </div>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Apple MacBook Pro 17"
+                </th>
+                <td class="py-4 px-6">
+                    Sliver
+                </td>
+                <td class="py-4 px-6">
+                    Laptop
+                </td>
+                
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Microsoft Surface Pro
+                </th>
+                <td class="py-4 px-6">
+                    White
+                </td>
+                <td class="py-4 px-6">
+                    Laptop PC
+                </td>
+               
+            </tr>
+            <tr class="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Magic Mouse 2
+                </th>
+                <td class="py-4 px-6">
+                    Black
+                </td>
+                <td class="py-4 px-6">
+                    Accessories
+                </td>
+               
+            </tr>
+        </tbody>
+    </table>
+</div>
 
-            </div>
-          </div>
+<br/><br/><br/><br/>
+
+<div class="overflow-x-auto relative shadow-md sm:rounded-lg w-[55rem] ml-[20rem]">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-white-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="py-3 px-6">
+                    Project Name
+                </th>
+                <th scope="col" class="py-3 px-6">
+                    Project Description
+                </th>
+                <th scope="col" class="py-3 px-6">
+                    Availability 
+                </th>
+                <th scope="col" class="py-3 px-6">
+                    Project Category
+                </th>
+               
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Apple MacBook Pro 17"
+                </th>
+                <td class="py-4 px-6">
+                    Sliver
+                </td>
+                <td class="py-4 px-6">
+                    Laptop
+                </td>
+                <td class="py-4 px-6">
+                    $2999
+                </td>
+                
+            </tr>
+            <tr class="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Microsoft Surface Pro
+                </th>
+                <td class="py-4 px-6">
+                    White
+                </td>
+                <td class="py-4 px-6">
+                    Laptop PC
+                </td>
+                <td class="py-4 px-6">
+                    $1999
+                </td>
+               
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Magic Mouse 2
+                </th>
+                <td class="py-4 px-6">
+                    Black
+                </td>
+                <td class="py-4 px-6">
+                    Accessories
+                </td>
+                <td class="py-4 px-6">
+                    $99
+                </td>
+               
+            </tr>
+            <tr class="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Google Pixel Phone
+                </th>
+                <td class="py-4 px-6">
+                    Gray
+                </td>
+                <td class="py-4 px-6">
+                    Phone
+                </td>
+                <td class="py-4 px-6">
+                    $799
+                </td>
+                
+            </tr>
+            <tr>
+                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Apple Watch 5
+                </th>
+                <td class="py-4 px-6">
+                    Red
+                </td>
+                <td class="py-4 px-6">
+                    Wearables
+                </td>
+                <td class="py-4 px-6">
+                    $999
+                </td>
+               
+            </tr>
+        </tbody>
+    </table>
+</div>
 
 
-        <Footer/>
+
+
+
+
+
+
+        {/* Table for Project select */}
+
+
+
         </div>
+        
         </>
       );
     };
     
     export default MatchedSupervisors;
+
+
+
+
+
+{/* <div id="back">
+        
+       
+
+          {/* US-01 Pasindu Vinod*/}
+          
+        //   <div style={{color:"white",width:"100%"}}>
+        //     <p className="text-center text-2xl font-semibold ml-10 mt-5">Place Bids for Supervisors {batchID}</p>
+        //     <br/>
+        //     <div id="Supervisors" style={{display:"in"}}>
+        //       <div>
+        //         <caption className="text-center mt-5">
+        //           Select supervisor
+        //         </caption>
+        //       <form onSubmit={SubmitBidding}>
+        //       <table id = "stable">
+                
+                 
+        //           {SupervisorName.map (supervisor => 
+        //           <tr>
+        //             <td><input type="checkbox" value={supervisor[1][0]} name="supervisor" onChange={(e) => {updateSupervisors(e)}}></input></td>
+        //             <td>{supervisor[1][1]}</td>
+        //             <td>{supervisor[1][2]}/{batchType}</td>
+        //           </tr>
+        //           )}
+                
+        //     </table>
+           
+
+           
+           
+        //       <h3 className="ml-15 mt-5">
+        //       Select Project
+        //       </h3>
+        //       <table style={{width:"700px"}}>
+           
+
+        //         <tr>
+        //           <th style={{width:"8rem"}}>Project Category </th>
+        //           <th style={{width:"rem"}}>Project Name</th>
+        //           <th style={{width:"6rem"}}>Project Description</th>
+        //           <th style={{width:"6rem"}}></th>
+
+        //         </tr>
+              
+
+        //         {Projects.map (project =>{  
+        //           if(project[1].projectStatus == false){
+        //             return(
+        //             <tr>
+        //               <td>{project[1].projectType}</td>
+        //               <td>{project[1].projectName}</td>
+        //               <td>{project[1].projectDescription}</td>
+        //               <td>
+        //                 <input type="radio" name="project" value={project[1]._id} onChange={(e) => setSelectedProject(e.target.value)}/> 
+        //               </td>
+        //             </tr>
+        //             )
+        //           }else{
+        //             return(
+        //             <tr>
+        //               <td>{project[1].projectType}</td>
+        //               <td>{project[1].projectName}</td>
+        //               <td>{project[1].projectDescription}</td>
+        //               <td>
+        //                 Taken 
+        //               </td>
+        //             </tr>
+        //             )
+        //           }                          
+                               
+        //           })}
+                
+
+
+        //       {/* </select>  */}
+        //       </table>
+
+              
+        //       <button type="submit">Place Bid</button>
+
+             
+
+              
+        //     </form>
+        //     </div>
+
+        //     </div>
+        //   </div>
+
+
+      
+        // </div> */}
