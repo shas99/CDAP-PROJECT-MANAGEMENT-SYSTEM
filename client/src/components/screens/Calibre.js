229c82257bcb4a3ea960bc1c9a293e7a52015f39
chathushkarodrigo <chathushkarodrigo@gmail.com>
2022-09-02 19:48:12 +0530

    divided sections

--- client/src/components/screens/Calibre.js
+++ client/src/components/screens/Calibre.js
@@ -1,11 +1,53 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react'
+
 
 function Calibre() {
     return (
 
-        <div>
-            dcnkdncd
-        </div>
+      
+   <div className=' bg-gray-900'>
+    
+    <div id='navigation-bar' className=' h-[7.2rem] bg-slate-50'>
+        Chamath
+    </div>
+
+    <div id='hero-section' className='   h-[40rem] bg-gray-900'>
+        Chathushka 
+
+        <h1 className=' text-7xl text-slate-50 font-extrabold mt-[6rem] ml-[10rem] '>
+            Lets get started !<br/> Its now or never
+        </h1>
+<br/>
+        <p className=' text-2xl  font-bold text-gray-500  ml-[10rem]'>
+            CDAP platform  empowers project roadmap <br/>to actual motivated software,  Inspiring students and lecturers 
+        </p>
+        <br/><br/><br/>
+       <div>
+            <img  src="https://media-exp2.licdn.com/dms/image/C560BAQEIerI4wDXA3w/company-logo_200_200/0/1643683606424?e=2147483647&v=beta&t=9SnPqaf6jG4vAoup_dlhdanepjK2ALMhU-Qicgjz0ZY" width="200" height="200" className='ml-[10rem]  rounded w-[5rem] h-[5rem]' />
+            </div>
+        
+
+        <button className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ml-[25rem]  mt-[rem]'> <a href='https://www.sliit.lk/'>Visit SLIIT Portal</a>  </button>
+    </div>
+
+    <div id='information-section' className='bg-slate-600 h-[30rem]' >
+        Pasindu
+    </div>
+    <div id='services-section' className='bg-slate-700 h-[30rem]' >
+        Shasvathan
+    </div>
+
+    <div id='below-section' className='bg-slate-800 h-[30rem]' >
+        Buddisha
+    </div>
+
+
+
+   </div>
+    
+ 
+ 
         
     )
 }
