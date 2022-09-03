c82a68edd01b1039e6441d3345e376771fc31dea
buddhi549 <humbleandwise16@gmail.com>
2022-09-03 12:22:06 +0530

    Initial Welcome page styling

--- client/src/components/screens/Calibre.js
+++ client/src/components/screens/Calibre.js
@@ -1,5 +1,15 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react'
+import "./Calibrebuddhisha.css"
+// import {
+//     MDBFooter,
+//     MDBContainer,
+//     MDBIcon,
+//     MDBInput,
+//     MDBCol,
+//     MDBRow,
+//     MDBBtn
+//   } from 'mdb-react-ui-kit';
 
 
 function Calibre() {
@@ -39,13 +49,180 @@ function Calibre() {
     </div>
 
     <div id='below-section' className='bg-slate-800 h-[30rem]' >
+
         Buddisha
+        <p className="footer">Calibre project management system is dedicated to helping students and lectures efficiently manage
+        the 4th year SLIIT Research projects , and provides many services and facilities such as topic registration, submission configurations
+        , group registration, rubric marking , teams meeting scheduling and many more. 
+
+        
+        
+        
+        
+         </p>
+         <br></br>
+         <p className="footer">You can sign up as a student, staff member or admin</p>
+         
+
+         <div lg='3' md='6' className='mb-4 mb-md-0'>
+              <h5  className='text-uppercase ml-[50rem] text-white'> Useful Links</h5>
+              <br></br>
+
+              <ul className='list-unstyled mb-0 ml-[70rem]'>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 2
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 3
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 4
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 5
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 6
+                  </a>
+                </li>
+              </ul>
+              </div>
+
+              <div  lg='3' md='6' className='mb-4  md:align-top'>
+           
+
+              <ul className='list-unstyled mb-0 mt-[-10rem] ml-[50rem]'>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 2
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 3
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 4
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 5
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 6
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+
+            <div  lg='3' md='6' className='mb-4 mt-[-10rem] md:align-top'>
+           
+
+              <ul className='list-unstyled mb-0 mt-[-7rem] ml-[30rem]'>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 2
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 3
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 4
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 5
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 6
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+           
+
+
+            </div>
+            <br></br>
+
+            <div class="grid md:grid-cols-3 text-white gird-cols-1 gap-4 flex justify-center mt-[-8rem] ml-[20rem] items-center">
+          <div class="md:ml-auto md:mb-6">
+            <p class="">
+              <strong>Sign up for our newsletter</strong>
+            </p>
+          </div>
+          </div>
+
+          <div class="md:mb-6 ml-[45rem] mt-[-4rem]" >
+            <input
+              type="text"
+              class="
+                form-control
+                block
+                w-10px
+                px-3
+                py-1.5
+                text-base
+                font-normal
+                text-gray-700
+                bg-white bg-clip-padding
+                border border-solid border-gray-300
+                rounded
+                transition
+                ease-in-out
+                m-0
+                focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+              "
+              id="exampleFormControlInput1"
+              placeholder="Email address"/>
+          </div>
+
+
+          
+         
     </div>
 
 
 
-   </div>
-    
+  
  
  
         
