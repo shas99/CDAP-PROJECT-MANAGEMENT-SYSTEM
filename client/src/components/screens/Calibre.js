ba20bc272392d6742e746f177468bfae0a561022
Shasvathan <pshasvathan1999@gmail.com>
2022-09-03 15:12:32 +0530

    merge conflict

--- client/src/components/screens/Calibre.js
+++ client/src/components/screens/Calibre.js
@@ -18,8 +18,32 @@ function Calibre() {
       
    <div className=' bg-gray-900'>
     
-    <div id='navigation-bar' className=' h-[7.2rem] bg-slate-50'>
-        Chamath
+    <div id='navigation-bar' className=' h-[5rem] bg-gradient-to-r from-blue-900 mt-50rem'>
+    <nav class="p-3 bg-gradient-to-r from-blue-900 border-gray-200 dark:bg-gray-800 dark:border-gray-700 h-[5rem]">
+  <div class="container flex flex-wrap justify-between items-center mx-auto">
+    <a href="#" class="flex items-center">
+        <img src="https://media.discordapp.net/attachments/929308623853723678/994252147799625768/Screenshot_2022-07-06_at_20.12.37.png" className="mr-3 h-6 sm:h-10 rounded" alt="Flowbite Logo" />
+        <span class="self-center text-xl font-semibold whitespace-nowrap text-white font-sans text-4xl font-bold">Calibre</span>
+    </a>
+    <button data-collapse-toggle="navbar-solid-bg" type="button" class="inline-flex justify-center items-center ml-3 text-gray-400 rounded-lg md:hidden hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300 dark:text-gray-400 dark:hover:text-white dark:focus:ring-gray-500" aria-controls="navbar-solid-bg" aria-expanded="false">
+      <span class="sr-only">Open main menu</span>
+      <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+    </button>
+    <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+      <ul class="flex flex-col mt-4 bg-gray-50 rounded-lg md:flex-row md:space-x-1 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+        <li>
+          <button className='text-white bg-[#24292F] hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2'><a href="/adminLogin" aria-current="page">Login As Admin</a></button>
+        </li>
+        <li>
+          <button className='text-white bg-[#24292F] hover:hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2'><a href="/stafflogin">Login As Staff</a></button>
+        </li>
+        <li>
+        <button className='text-white bg-[#24292F] hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2'><a href="/login">Login As Student</a></button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
     </div>
 
     <div id='hero-section' className='   h-[40rem] bg-gray-900'>
@@ -63,8 +87,37 @@ function Calibre() {
         </div>
     </div>
 
-    <div id='information-section' className='bg-slate-600 h-[30rem]' >
-        Pasindu
+    <div id='information-section' className='bg-slate-600 w-full  h-[auto] border-2 border-solid border-blue-900 mt-[16rem]' >
+        <div className="inline-block w-full h-auto border-2 border-solid border-red-900"> 
+            <div className="flex lg:w-2/3 float-left border-solid border-2 border-green-600">
+            <img src="https://cdn.discordapp.com/attachments/929308623853723678/1015512673384939540/Milestones.jpg" width="94%" height="auto" style={{marginLeft:"3%", marginTop:"20px", marginBottom:"20px"}}></img>
+
+            </div>
+            <div className="flex lg:w-1/3 float-right border-solid border-2">
+            <p className="w-full h-full">This is sample data for now</p>
+            </div>
+        </div>    
+
+        <div className="inline-block w-full h-auto border-2 border-solid border-red-900"> 
+            <div className="flex lg:w-2/5 float-left border-solid border-2">
+            <p className="w-full h-full">This is sample data for now</p>
+            </div>
+
+            <div className="flex lg:w-3/5 float-right border-solid border-2 border-green-600">
+            <img src="https://cdn.discordapp.com/attachments/929308623853723678/1015512673619824700/Assesments.jpg" className="" width="90%"  style={{marginBottom:"15px", marginLeft:"5%"}}></img>
+
+            </div>
+            
+        </div>    
+        {/* <div className="w-full border-2 border-solid border-red-900">
+            <div className="flex lg:w-2/3 float-right border-solid border-2 h-auto">
+            <img src="https://cdn.discordapp.com/attachments/929308623853723678/1015512673619824700/Assesments.jpg"  className="w-80% h-auto"></img>
+
+            </div>
+            <div className="flex lg:w-1/3 float-left border-solid border-2">
+            <p className="w-full h-full">This is sample data for now</p>
+            </div>
+        </div>     */}
     </div>
     <div id='services-section' className='bg-slate-700 h-[30rem]' >
      &nbsp;
