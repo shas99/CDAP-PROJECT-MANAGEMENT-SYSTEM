a6da45c55c9597ae02e6f7540274915a0cb63aaa
ChathushkaRodrigo <chathushkarodrigo@gmail.com>
2022-12-26 11:31:14 +0530

    All screens refined

--- client/src/components/screens/MarkingScreen.js
+++ client/src/components/screens/MarkingScreen.js
@@ -128,10 +128,10 @@ const Marking = ({history}) =>{
                     <li class="mb-3 font-normal text-gray-700 dark:text-gray-400" ><b>Description</b>: &nbsp;&nbsp;&nbsp;&nbsp;{submission.Description}</li>&nbsp;
                     <li class="mb-3 font-normal text-gray-700 dark:text-gray-400" ><b>Heading    </b>: &nbsp;&nbsp;&nbsp;&nbsp;{submission.Heading}</li>&nbsp;
                     <label>
-              
-                    <div class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> <a href={`/markRubric/${submission._id}`}>Mark</a></div>&nbsp;&nbsp;
-
-    </label>
+                    <div class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> 
+                      <a href={`/markRubric/${submission._id}`}>Mark</a>
+                    </div>&nbsp;&nbsp;
+      </label>
 
       </div>
    
